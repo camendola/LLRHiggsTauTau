58b8b13dabcfe8b3a96d5dd053ae1bb6e596a3e6
camendola <chia.amendola@gmail.com>
2016-12-16 16:09:46 +0100

    crab template

--- NtupleProducer/test/crab3_template.py
+++ NtupleProducer/test/crab3_template.py
@@ -4,7 +4,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'DefaultReqName'
+config.General.requestName = 'GluGluToRadionToHHTo2B2Tau_M-250'
 config.General.workArea = 'DefaultCrab3Area'
 
 config.section_("JobType")
@@ -12,14 +12,14 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'analyzer.py' # to produce LLR ntuples or EnrichedMiniAOD according to the RunNtuplizer bool
 
 config.section_("Data")
-config.Data.inputDataset = '/my/precious/dataset'
+config.Data.inputDataset = 'root://xrootd-cms.infn.it//GluGluToRadionToHHTo2B2Tau_M-250_narrow_13TeV-madgraph/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 20000 #number of events per jobs # 18K FOR SINGLE ELE, 10k for others
+config.Data.unitsPerJob = 10000 #number of events per jobs # 18K FOR SINGLE ELE, 10k for others
 config.Data.totalUnits = -1 #number of event
-config.Data.outLFNDirBase = '/store/user/lcadamur/HHNtuples/DefaultOutLFNDirBase'
+config.Data.outLFNDirBase = '/store/user/camendol/HHNtuples/GluGluToRadionToHHTo2B2Tau_M-250'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'DefaultPublishName'
+config.Data.outputDatasetTag = 'GluGluToRadionToHHTo2B2Tau_M-250'
 
 config.section_("Site")
 config.Site.storageSite = 'T2_FR_GRIF_LLR'
